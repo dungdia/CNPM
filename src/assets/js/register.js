ff931ea6ee33e7b901b68da7346e51016bf55fd0
phusomnia <tuanhphu2004thcs@gmail.com>
2024-10-29 13:21:53 -0400

    #30/10 cap nhat register

--- src/assets/js/register.js
+++ src/assets/js/register.js
@@ -1,14 +1,51 @@
 import { Validation } from './validation.js'
 
+const formRegister = document.getElementById("register-form__body")
 const signupEvent = document.getElementById("register-button__sign-up")
 const signupFormBody = document.getElementById("register-form__body").querySelectorAll("input")
 const signupInputUsername = document.getElementById("register-form__input--username")
-const signupInputEmail = document.getElementById("register-form__input--email")
 const signupInputPassword = document.getElementById("register-form__input--password")
 const signupInputConfirmPassword = document.getElementById("register-form__input--confirm-password")
 
-signupEvent.addEventListener('click', () => {
-    console.log(Validation.isFormBlank(signupFormBody))
-    console.log(Validation.regexEmail(signupInputEmail))
-    console.log(Validation.checkConfirmPassword(signupInputPassword, signupInputConfirmPassword))
-})  
+formRegister.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    let valid = true;
+    if (Validation.isFormBlank(signupFormBody) &&
+        Validation.checkConfirmPassword(signupInputPassword, signupInputConfirmPassword)) {
+        await registerData();
+    }
+})
+
+let registerData = async () => {
+    var formData = new FormData(formRegister);
+    var data = Object.fromEntries(formData.entries());
+    try {
+        const res = await fetch(`/api/data/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        if (!res.ok) {
+            const errorMessage = await res.text();
+            throw new Error(`HTTP error! status: ${res.status} - ${errorMessage}`);
+        }
+
+        const resData = await res.json();
+        if (resData.message === "1") {
+            alert("Đăng ký thành công")
+            // thành công thì chuyển sang login form
+            window.location.href = "/login"
+        }
+        else if (resData.message === "0") {
+            alert("Tên tài khoản đã tồn tại")
+        }
+        else {
+            alert("Đăng ký thất bại")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
