8601570c715efe390a0c1c2fed78109bffdb1cc7
Quangdung090 <ttramquangdung@gmail.com>
2024-12-08 21:28:29 +0700

    validation fixed

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ async function apiHandler() {
   const folders = await getAllFile(path.join(__dirname, "app", "api"));
 
   for (const folder of folders) {
-    const parent = folder.split("/").pop();
+    const parent = folder.split("\\").pop();
     await setUpREST(app, path.join(__dirname, "app", "api"), parent);
   }
 }
@@ -42,7 +42,7 @@ async function viewsHandler() {
   for (const page of viewFolder) {
     //lấy tên trang để setup đường dẫn
 
-    const pageName = page.split("/").pop().replace(".ejs", "");
+    const pageName = page.split("\\").pop().replace(".ejs", "");
     app.get(`/${pageName}`,requireLoginPage.includes(pageName) ? authMiddleWare : (req,res,next)=> {next()}, (req, res) => {
 
       res.render(page);
@@ -51,7 +51,7 @@ async function viewsHandler() {
 
 
   for(const page of adminFolder){
-    const pageName = page.split("/").pop().replace(".ejs", "");
+    const pageName = page.split("\\").pop().replace(".ejs", "");
     app.get(`/admin/${pageName}`,authMiddleWare,roleMiddleWare,(req,res)=>{
     res.render(page)
   })
